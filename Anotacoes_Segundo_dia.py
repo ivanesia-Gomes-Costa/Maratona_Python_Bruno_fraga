0a4197b33491888dd08648d71d97842118512f15
Ivanesia Gomes <ivanesiagomes@hotmail.com>
2021-09-11 00:53:14 -0300

    Alteração nos arquivos

---
+++ Anotacoes_Segundo_dia.py
@@ -0,0 +1,27 @@
+def welcome(name):
+    msg = 'Ola ' + name.title()
+    print(msg)
+
+
+welcome('ivanesia gomes costa')
+welcome('lucas alberto de souza')
+
+#criando uma função de soma
+def soma(n1, n2):
+    calculo = n1 + n2
+    print(calculo)
+
+#criando uma função de subtração
+def subtracao(s1, s2):
+    calculo_sub = (s1 - s2)
+    print(calculo_sub)
+
+soma (15, 15)
+
+subtracao (15, 15)
+
+
+valor = int(input('Digite o valor de n1:  '))
+segundo_valor = int(input('Digite o segundo valor: '))
+
+soma(valor, segundo_valor)
